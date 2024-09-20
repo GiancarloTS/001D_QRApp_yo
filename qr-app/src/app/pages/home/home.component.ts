8b68d9b5b49b3f51fd3490045a5198ae8514fe40
GiancarloTS <164694231+GiancarloTS@users.noreply.github.com>
2024-09-20 20:55:17 -0300

    quitando if y arreglando detallitos
    Taopaipai 4.20

--- qr-app/src/app/pages/home/home.component.ts
+++ qr-app/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 import { CuentaService } from 'src/app/services/cuenta.service';
 
 @Component({
@@ -8,16 +9,21 @@ import { CuentaService } from 'src/app/services/cuenta.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent  implements OnInit {
-  cuenta = inject(CuentaService);
-  user! : String | null;
-  subscripcionCuenta!: Subscription;
+  auth = inject(AuthService);
+  subscriptionAuthService!: Subscription; // Subscripción para el observable del estado de autenticación
+
+
+
+
+  user! : String ;
 
   constructor() { }
 
   ngOnInit() {
-    this.subscripcionCuenta = this.cuenta.user$.subscribe(cuenta => {
-      this.user = cuenta;
-    })
+    this.subscriptionAuthService = this.auth.usuario$.subscribe(usuario => {
+      this.user = usuario
+      console.log('Header:', usuario);
+    }); // Obtiene el nombre del usuario logueado
   }
 
 }
